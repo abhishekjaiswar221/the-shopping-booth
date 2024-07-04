49db44d29597a7092c1d4b6bf07dd8eb63eda76f
Abhishek D Jaiswar <devabhishekjaiswar@gmail.com>
2024-07-04 17:02:38 +0530

    Add shadcn/ui component library and did some vite configurations

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     port: 3000,
   },
