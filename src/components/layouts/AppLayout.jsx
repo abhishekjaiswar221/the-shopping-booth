a7d95786fe3c0b35778edd16945977bbda2664a6
Abhishek D Jaiswar <113609441+abhishekjaiswar221@users.noreply.github.com>
2024-07-29 23:05:37 +0530

    Update AppLayout's main tag to put px-10 only on large screens

--- src/components/layouts/AppLayout.jsx
+++ src/components/layouts/AppLayout.jsx
@@ -14,7 +14,7 @@ const AppLayout = () => {
       <Provider store={store}>
         {/* Your header content */}
         {!hideHeaderFooter && <HeaderNavigation />}
-        <main className="px-20">
+        <main className="lg:px-20">
           {/* Child routes will render here */}
           <Outlet />
           <Toaster />
