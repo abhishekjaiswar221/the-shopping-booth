90319ad08def322b7a70a37d99dcd055537a218a
Abhishek D Jaiswar <devabhishekjaiswar@gmail.com>
2024-10-05 16:54:58 +0530

    Make some UI fixes

--- src/components/layouts/AppLayout.jsx
+++ src/components/layouts/AppLayout.jsx
@@ -14,7 +14,7 @@ const AppLayout = () => {
       <Provider store={store}>
         {/* Your header content */}
         {!hideHeaderFooter && <HeaderNavigation />}
-        <main className="bg-[#f7f7f7]">
+        <main>
           {/* Child routes will render here */}
           <Outlet />
           <Toaster />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -62,7 +62,7 @@ const Home = () => {
         {/* <HeroSection /> */}
       </div>
       <div className="px-5 space-y-10 lg:px-10 xl:px-20">
-        <div className="lg:flex lg:items-center lg:justify-between">
+        <div className="px-6 lg:flex lg:items-center lg:justify-between">
           <div>
             <h3 className="text-2xl font-semibold tracking-tight scroll-m-20">
               Popular Products
@@ -105,14 +105,14 @@ const Home = () => {
         <div className="p-6 bg-white md:p-8 xl:p-6 rounded-xl">
           <ProductSection />
         </div>
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between px-6">
           <div>
-            <p className="text-lg text-muted-foreground">
+            <p className="text-sm md:text-base text-muted-foreground">
               1604 trending products <br /> in 16 categories
             </p>
           </div>
           <div>
-            <Button asChild className="rounded-full w-44">
+            <Button asChild className="w-24 rounded-full md:w-44">
               <Link to={"/cart"}>
                 See all
                 <ArrowRight
