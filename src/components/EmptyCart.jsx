472ef7ec37400baccb367d5180f73cd21bbf7a8c
Abhishek D Jaiswar <devabhishekjaiswar@gmail.com>
2025-01-09 18:02:31 +0530

    Update the empty cart ui and make it responsive

--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -4,19 +4,24 @@ import { Link } from "react-router-dom";
 
 const EmptyCart = () => {
   return (
-    <div className="py-20">
-      <div className="flex items-center justify-center h-[645px]">
-        <div className="flex flex-col items-center gap-1 text-center">
-          <h3 className="text-2xl font-bold tracking-tight">
-            You have no products
-          </h3>
-          <p className="text-sm text-muted-foreground">
-            Looks like you haven&apos;t added anything to your cart yet
-          </p>
-          <Button className="mt-4 rounded-lg" asChild>
-            <Link to={"/"}>Add Product</Link>
-          </Button>
+    <div className="flex items-center justify-center h-screen px-4 lg:px-10 xl:px-20">
+      <div className="flex flex-col items-center justify-center gap-3">
+        <div>
+          <img
+            className="w-full h-52"
+            src="/images/empty-cart.webp"
+            alt="Empty Cart Illustrated Image"
+          />
         </div>
+        <h3 className="text-3xl font-bold tracking-tight xl:text-5xl lg:text-4xl">
+          Your Cart is Empty!
+        </h3>
+        <p className="text-sm text-zinc-500">
+          Looks like you haven&apos;t added anything to your cart yet!
+        </p>
+        <Button size="lg" className="mt-4 rounded-xl" asChild>
+          <Link to={"/"}>Add Product</Link>
+        </Button>
       </div>
     </div>
   );
